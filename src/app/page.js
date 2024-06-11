d61295f46a2f7191a5c0bd0f136f937854313c85
Your Name <you@example.com>
2024-06-11 22:04:45 +0600

    remove-getdata

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,6 @@ import Alldata from "@/components/Alldata";
 import Image from "next/image";
 
 export default async function Home() {
-  const user = await getData();
   return (
     <main>
       <Alldata user={user} />
